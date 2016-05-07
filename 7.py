ccdd44fb9103bd853c35a5c927a538b9453a3f1c
qijiayin <qijiayin.l@gmail.com>
2016-05-07 16:03:40 -0700

    7 写的很ugly

--- 7.py
+++ 7.py
@@ -12,10 +12,21 @@ class Solution(object):
             x *= -1
         else :
             flag = 1
-        MAXINT = 2147483648
+        MAXINT = 2**31 -1
+        MININT = 2**31 * -1
+
         res = 0
         while x > 0 :
+            if res == MAXINT / 10:
+                if (flag > 0 and (x % 10) > 7 ) or (flag < 0 and (x % 10) > 8):
+                    return 0
+
+            elif res > MAXINT / 10:
+                return 0
+
             res *= 10
             res += (x % 10)
             x /= 10
+            print x
+
         return res * flag
