6d857aa79db2ae3e2fd623e7cbd1c7425cbf61c5
qijiayin <qijiayin.l@gmail.com>
2016-05-07 16:25:25 -0700

    303 语法不对

--- 303.py
+++ 303.py
@@ -1,30 +1,16 @@
 #303 range-sum-query-immutable/
 class NumArray(object):
-    def getTotleSum(self, nums):
-        res = 0
-        for i in range(len(nums)):
-            res += nums[i]
-        return res
 
-    def getBeginSum(self,nums):
+
+    def getSumList(self,nums):
         res = []
         if len(nums) == 0 :
             return res
         res.append(nums[0])
         for i in range(1, len(nums)):
             res.append(res[i-1] + nums[i])
-        print res
         return res
 
-    def getEndSum(self,nums):
-        res = []
-        if len(nums) == 0 :
-            return res
-        res.append(nums[-1])
-        for i in range(len(nums) - 2 , 0):
-            res.append(res[i + 1] + nums[i])
-        print res
-        return res
 
 
     def __init__(self, nums):
@@ -32,9 +18,9 @@ class NumArray(object):
         initialize your data structure here.
         :type nums: List[int]
         """
-        self.totle_sum = self.getTotleSum(nums)
-        self.from_begin_sum = self.getBeginSum(nums)
-        self.from_end_sum = self.getEndSum(nums)
+        self.sumlist = []
+        self.sumlist = self.getSumList(nums)
+
 
 
 
@@ -45,11 +31,9 @@ class NumArray(object):
         :type j: int
         :rtype: int
         """
-        return self.totle_sum - self.from_begin_sum[i] - self.from_end_sum[j]
-
+        return  self.sumlist(j) -  self.sumlist(i-1)
 
 # Your NumArray object will be instantiated and called as such:
-nums = [[-2,0,3,-5,2,-1]]
-numArray = NumArray(nums)
-print numArray.sumRange(0, 1)
+# numArray = NumArray(nums)
+# numArray.sumRange(0, 1)
 # numArray.sumRange(1, 2)
