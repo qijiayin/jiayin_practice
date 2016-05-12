677e5f35ee58496c52d25f1edb3a53cb0e40750a
qijiayin <qijiayin.l@gmail.com>
2016-05-11 22:18:02 -0700

    136

---
+++ 136.py
@@ -0,0 +1,11 @@
+#136 single-number/
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 0
+        for i in range(len(nums)):
+            res ^= nums[i]
+        return res
