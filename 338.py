0c3c1bd7cb2fc02500071a00fccc0824244c14e6
qijiayin <qijiayin.l@gmail.com>
2016-05-11 21:36:40 -0700

    338

---
+++ 338.py
@@ -0,0 +1,17 @@
+#338 counting-bits/
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+
+        res = [0]
+        for i in range(1, num+1):
+            if i % 2 == 0:
+                res.append(res[i/2])
+            else:
+
+                res.append(res[i/2] + 1)
+
+        return res
