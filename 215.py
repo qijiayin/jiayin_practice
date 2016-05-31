2dec4de446c1eb44b416a491f7b175cf43ddad98
qijiayin <qijiayin.l@gmail.com>
2016-05-31 00:07:23 -0700

    215 还有更快的做法么？

--- 215.py
+++ 215.py
@@ -1,1 +1,23 @@
 #215 kth-largest-element-in-an-array/
+class Node(object):
+    def __init__(self, x):
+        self.val = x
+    def __lt__(self, other):
+        return self.val > other.val
+
+class Solution(object):
+
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        idx = 0
+        heap = [ Node(x) for x in nums]
+        heapq.heapify(heap)
+        res = Node(nums[0])
+        while idx < k :
+            res = heapq.heappop(heap)
+            idx += 1
+        return res.val
