b61ccc236c73ed2a2314d9005ec94c79cdd8197b
qijiayin <qijiayin.l@gmail.com>
2016-05-31 00:31:52 -0700

    215 这才是正确的写法

--- 215.py
+++ 215.py
@@ -1,4 +1,30 @@
 #215 kth-largest-element-in-an-array/
+
+class Solution(object):
+
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        heap = []
+        n = 0
+
+        for x in nums :
+            if n < k :
+                heapq.heappush(heap, x )
+                n += 1
+
+            else:
+                t = heap[0]
+                if x > t :
+                    heapq.heappop(heap)
+                    heapq.heappush(heap, x )
+
+        return heap[0]
+
+        
 class Node(object):
     def __init__(self, x):
         self.val = x
