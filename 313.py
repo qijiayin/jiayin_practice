b3af1a60ed88410114cabfefe8125c62e3f147b1
qijiayin <qijiayin.l@gmail.com>
2016-05-31 00:07:08 -0700

    313

--- 313.py
+++ 313.py
@@ -1,14 +1,15 @@
-#313
-'''
-Write a program to find the nth super ugly number.
+#313 super-ugly-number/
+import heapq
 
-Super ugly numbers are positive numbers whose all prime factors are in the given prime list primes of size k. For example, [1, 2, 4, 7, 8, 13, 14, 16, 19, 26, 28, 32] is the sequence of the first 12 super ugly numbers given primes = [2, 7, 13, 19] of size 4.
+class Node(object):
+    def __init__(self, value, index, prime):
+        self.value = value
+        self.index = index
+        self.prime = prime
 
-Note:
-(1) 1 is a super ugly number for any given primes.
-(2) The given numbers in primes are in ascending order.
-(3) 0 < k ≤ 100, 0 < n ≤ 106, 0 < primes[i] < 1000.
-'''
+    def __lt__(self, other):
+        return self.value < other.value
+
 class Solution(object):
     def nthSuperUglyNumber(self, n, primes):
         """
@@ -16,27 +17,27 @@ class Solution(object):
         :type primes: List[int]
         :rtype: int
         """
-        dict = {x:True for x in primes}
+        if n == 1:
+            return 1
+
         res = [1]
-        count = 1
-        num = 2
+        heap = []
+        [heapq.heappush(heap, Node(x, 0, x)) for x in primes]
 
-        while count < n :
-            val = dict.get(num)
-            if val != None:
-                res.append(num)
-                count += 1
-            else:
-                for x in primes[::-1]:
+        while len(res) < n:
+            next = heap[0]
+            res.append(next.value)
+            while len(heap) > 0 and next.value == res[-1]:
+                heapq.heappop(heap)
+                index = next.index + 1
+                cur = Node(res[index] * next.prime, index, next.prime)
+                print  next.value, next.prime, index, res[index],cur.value
+                print res
 
-                    if num % x == 0 and dict.get(num/x) != None:
-                        dict[num] = True
-                        res.append(num)
-                        count += 1
-                        break
+                heapq.heappush(heap, cur)
+                next = heap[0]
 
-
-            num += 1
-
-
-        return res[n-1]
+        return res[-1]
+s=Solution()
+primes=[2,3,5]
+print s.nthSuperUglyNumber(10,primes)
