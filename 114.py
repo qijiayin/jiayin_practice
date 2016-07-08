7a96a04d71d9122fe83d586ff0ef80e5f65d119a
qijiayin <qijiayin.l@gmail.com>
2016-07-07 22:37:20 -0700

    114 对了

--- 114.py
+++ 114.py
@@ -7,25 +7,23 @@
 #         self.right = None
 
 class Solution(object):
-    def flatImp(self, root):
-        if root == None:
-            return None
-        left = right = None
-        if root.left != None:
-            left = self.flatten(root.left)
-        if root.right != None:
-            right = self.flatten(root.right)
-
-        if left != None:
-            print left.val
-            left.right = right
-            root.right = left
-            root.left = None
-        return root
 
     def flatten(self, root):
         """
         :type root: TreeNode
         :rtype: void Do not return anything, modify root in-place instead.
         """
-        root = self.flatImp(root)
+
+        if root == None:
+            return
+        self.flatten(root.left)
+        self.flatten(root.right)
+        if root.left == None:
+            return
+        tail = root.left
+        while tail.right != None:
+            tail = tail.right
+        tail.right = root.right
+        root.right = root.left
+        root.left = None
+            
